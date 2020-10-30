9c53b3d9b2dedae2e4393dec2fcba0142bb66b83
Mehdin Masinovic <54234125+MehdinMasinovic@users.noreply.github.com>
2020-10-30 11:36:57 +0100

    Repeat-Densities-per-STR

---
+++ Repeat-Densities-per-STR/git_repeat_densities_per_STR.R
@@ -0,0 +1,11 @@
+#Author: Mehdin Masinovic
+
+####Info####
+"
+
+ This folder contains all analysed short tandem repeats. 
+ All mono-, di-, and trinucleotide repeats are stratified by repeat length.
+ They are described by four types of plots, more explanation can be found in 
+ chapter 4.3. \"Statistical analysis and data visualization\".   
+
+"
